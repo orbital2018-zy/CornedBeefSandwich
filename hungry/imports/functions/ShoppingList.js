eb85ffe35909eaef411c61b94b9e3d4065ef9e26
DESKTOP-ELS9VJL\zhenying <e0191452@u.nus.edu>
2018-07-16 12:20:55 +0800

    local storage for shopping list is done

--- hungry/imports/functions/ShoppingList.js
+++ hungry/imports/functions/ShoppingList.js
@@ -56,7 +56,35 @@ if (Meteor.isClient) {
 
     UI.registerHelper("Items", function () {
         return Ingredients.find();
-      }); 
+    });
+
+    Template.ShoppingList.events({
+        'submit.new-ingredient'(event) {
+            event.preventDefault();
+    
+            const target = event.target;
+            const text = target.text.value;
+    
+            Ingredients.insert({
+                text,
+            });
+    
+            target.text.value='';
+        },
+    });
+
+    Template.ingredient.events({
+        'click.toggle-checked'() {
+            Ingredients.update(this._id, {
+                $set: { checked: !this.checked },
+            });
+        },
+    
+        'click.delete'() {
+            Ingredients.remove(this._id);
+            Ingredients.update();
+        },
+    });
 }
 
 /*Template.ShoppingList.events({
